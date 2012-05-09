7c7ad79e3973398557cbd5eda4767c7605bc5b73
Collin Boots <cboots@seas.upenn.edu>
2012-05-09 15:47:25 +0000

    Added MouseDroidActivityTest.java

---
+++ src/com/cfms/android/mousedroid/free/test/MouseDroidActivityTest.java
@@ -0,0 +1,39 @@
+package com.cfms.android.mousedroid.free.test;
+
+import com.cfms.android.mousedroid.MouseDroidActivity;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+public class MouseDroidActivityTest extends
+		ActivityInstrumentationTestCase2<MouseDroidActivity> {
+
+	MouseDroidActivity mActivity;
+	
+	public MouseDroidActivityTest()
+	{
+		super("com.cfms.android.mousedroid.free", MouseDroidActivity.class);
+	}
+	
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        mActivity = this.getActivity();
+        
+    }
+    
+    public void testPreconditions() {
+    	assertNotNull(mActivity);
+    	
+    }
+    
+    public void testAppName(){
+    	String app_name = mActivity.getString(com.cfms.android.mousedroid.free.R.string.app_name);
+    	assertEquals("Mouse Droid", app_name);
+    }
+    
+    public void testAppPackage(){
+    	assertEquals(mActivity.getApplicationInfo().packageName, "com.cfms.android.mousedroid.free");
+    }
+	
+	
+}
