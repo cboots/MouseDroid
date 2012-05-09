fbc99efd1b34075dc656a75c497b4434947a5662
Collin Boots <cboots1990@gmail.com>
2012-05-09 00:03:18 +0000

    Initial Project Commit

---
+++ src/com/cfms/android/mousedroid/MouseDroidActivity.java
@@ -0,0 +1,13 @@
+package com.cfms.android.mousedroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MouseDroidActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
