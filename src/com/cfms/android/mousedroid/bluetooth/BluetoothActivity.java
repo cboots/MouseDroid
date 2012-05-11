30a1e713a72372fc1552d3ad742332a31df8339a
Collin Boots <cboots@seas.upenn.edu>
2012-05-11 02:07:44 +0000

    Added some bluetooth code

--- src/com/cfms/android/mousedroid/bluetooth/BluetoothActivity.java
+++ src/com/cfms/android/mousedroid/bluetooth/BluetoothActivity.java
@@ -1,16 +1,92 @@
 package com.cfms.android.mousedroid.bluetooth;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 
 import com.cfms.android.mousedroid.activity.BaseActivity;
+import com.cfms.android.mousedroid.bluetooth.BluetoothService.BluetoothBinder;
 
 
 public abstract class BluetoothActivity extends BaseActivity {
 
+	protected BluetoothService mBtService;
+    protected boolean mBound = false;
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
-		
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+	}
+
+	@Override
+	public void onPause() {
+		super.onPause();
+	}
+
+	@Override
+	public void onStart() {
+		super.onStart();
+
+        // Bind to LocalService
+        Intent intent = new Intent(this, BluetoothService.class);
+        bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
+	}
+
+	@Override
+	public void onStop() {
+		// Unbind from the service
+        if (mBound) {
+            unbindService(mConnection);
+            mBound = false;
+            mBtService = null;
+        }
+        
+		super.onStop();   
+	}
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
 	}
 	
+    /** Defines callbacks for service binding, passed to bindService() */
+    private ServiceConnection mConnection = new ServiceConnection() {
+
+        @Override
+        public void onServiceConnected(ComponentName className,
+                IBinder service) {
+            // We've bound to LocalService, cast the IBinder and get LocalService instance
+        	BluetoothBinder binder = (BluetoothBinder) service;
+        	mBtService = binder.getService();
+            mBound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName arg0) {
+            mBound = false;
+            mBtService = null;
+        }
+    };
+	
+    public boolean isBoundToBTService()
+    {
+    	return mBound;
+    }
+    
+    public BluetoothService getBTService()
+    {
+    	if(isBoundToBTService())
+    	{
+    		return mBtService;
+    	}
+    	return null;
+    }
 }
--- src/com/cfms/android/mousedroid/bluetooth/BluetoothService.java
+++ src/com/cfms/android/mousedroid/bluetooth/BluetoothService.java
@@ -2,14 +2,47 @@ package com.cfms.android.mousedroid.bluetooth;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
+import android.os.HandlerThread;
 import android.os.IBinder;
+
+import com.cfms.android.mousedroid.utils.DebugLog;
 
 public class BluetoothService extends Service {
 
-	@Override
-	public IBinder onBind(Intent arg0) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	 // Binder given to clients
+    private final IBinder mBinder = new BluetoothBinder();
+
+    /**
+     * Class used for the client Binder.  Because we know this service always
+     * runs in the same process as its clients, we don't need to deal with IPC.
+     */
+    public class BluetoothBinder extends Binder {
+    	BluetoothService getService() {
+            // Return this instance of LocalService so clients can call public methods
+            return BluetoothService.this;
+        }
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return mBinder;
+    }
+    
+    @Override
+    public void onCreate() {
+		  DebugLog.I("BluetoothService", "Service onCreate()");
+    }
+    
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        DebugLog.I("BluetoothService", "Service onStartCommand()");
+        return super.onStartCommand(intent,flags,startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        DebugLog.I("BluetoothService", "Service onDestroy()");
+    }
 
 }
