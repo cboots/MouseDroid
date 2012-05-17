c18b2b0c3beb5ade4e45c4e025571c683f6d59d7
Collin Boots <cboots@seas.upenn.edu>
2012-05-17 13:15:03 +0000

    Fixed UUID issue

--- src/com/cfms/mousedroid/pc/ListeningThread.java
+++ src/com/cfms/mousedroid/pc/ListeningThread.java
@@ -2,7 +2,6 @@ package com.cfms.mousedroid.pc;
 
 import javax.bluetooth.DiscoveryAgent;
 import javax.bluetooth.LocalDevice;
-import java.util.UUID;
 import javax.microedition.io.Connector;
 import javax.microedition.io.StreamConnection;
 import javax.microedition.io.StreamConnectionNotifier;
@@ -27,12 +26,18 @@ public class ListeningThread implements Runnable {
 		try {
 			local = LocalDevice.getLocalDevice();
 			local.setDiscoverable(DiscoveryAgent.GIAC);
-
-			UUID uuid = UUID
-					.fromString("fa46ddbb-0694-49f6-993c-1a1621f2e34d");
-			String url = "btspp://localhost:" + uuid.toString()
+			
+//			UUID uuid = new UUID(80087355); // "04c6093b-0000-1000-8000-00805f9b34fb"		
+//			String url = "btspp://localhost:" + uuid.toString()
+//					+ ";name=RemoteBluetooth";
+			
+			//Server string must not have any dashes in the ID.
+			String uuidStr = "fa46ddbb069449f6993c1a1621f2e34d";//fa46ddbb-0694-49f6-993c-1a1621f2e34d
+			
+			String url = "btspp://localhost:" + uuidStr
 					+ ";name=RemoteBluetooth";
 			notifier = (StreamConnectionNotifier) Connector.open(url);
+			
 
 		} catch (Exception e) {
 			e.printStackTrace();
