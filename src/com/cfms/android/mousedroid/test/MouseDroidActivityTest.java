76a73d3b5a1691a0c13fc04626b4655cb49807ab
Collin Boots <cboots@seas.upenn.edu>
2012-05-17 14:27:53 +0000

    Implemented bluetooth pairing and device connection test.  Fixed bug with bluetooth not binding correctly.

--- src/com/cfms/android/mousedroid/test/MouseDroidActivityTest.java
+++ src/com/cfms/android/mousedroid/test/MouseDroidActivityTest.java
@@ -1,8 +1,14 @@
 package com.cfms.android.mousedroid.test;
 
+import android.app.Activity;
+import android.app.Instrumentation;
+import android.app.Instrumentation.ActivityMonitor;
 import android.test.ActivityInstrumentationTestCase2;
+import android.view.KeyEvent;
 
+import com.cfms.android.mousedroid.R;
 import com.cfms.android.mousedroid.activity.MouseDroidActivity;
+import com.cfms.android.mousedroid.activity.DeviceListActivity;
 
 public class MouseDroidActivityTest extends
 		ActivityInstrumentationTestCase2<MouseDroidActivity> {
@@ -32,4 +38,27 @@ public class MouseDroidActivityTest extends
     	
     }
     
+    
+    public void testMenuItemScan(){
+    	ActivityMonitor am = getInstrumentation().addMonitor(DeviceListActivity.class.getName(), null, false);
+
+    	// Click the menu option
+    	getInstrumentation().sendKeyDownUpSync(KeyEvent.KEYCODE_MENU);
+    	getInstrumentation().invokeMenuActionSync(mActivity, R.id.scan, 0);
+
+    	Activity a = getInstrumentation().waitForMonitorWithTimeout(am, 1000);
+    	assertEquals(true, getInstrumentation().checkMonitorHit(am, 1));
+    	a.finish();
+    }
+    
+    public void testBluetoothBindingOnActivityResultBug(){
+    	getInstrumentation().callActivityOnPause(mActivity);
+    	getInstrumentation().callActivityOnStop(mActivity);
+    	getInstrumentation().callActivityOnStart(mActivity);
+    	getInstrumentation().callActivityOnResume(mActivity);
+    	assertEquals(true, mActivity.isBoundToBTService());
+    	assertNotNull(mActivity.getBTService());
+    	
+    	
+    }
 }
