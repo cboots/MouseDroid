a91b8805665d5ddb8c8f0ebcc5dc2efc4aaf5b8d
Collin Boots <cboots@seas.upenn.edu>
2012-05-18 22:05:58 +0000

    Implemented better threading on server side and beginnings of command protocol

---
+++ src/com/cfms/android/mousedroid/test/ProtocolTest.java
@@ -0,0 +1,31 @@
+package com.cfms.android.mousedroid.test;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+import com.cfms.android.mousedroid.activity.PreferencesActivity;
+import com.cfms.android.mousedroid.bluetooth.BluetoothProtocol;
+
+public class ProtocolTest extends
+		ActivityInstrumentationTestCase2<PreferencesActivity> {
+
+	PreferencesActivity mActivity;
+	
+	public ProtocolTest()
+	{
+		super(PreferencesActivity.class);
+	}
+	
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        mActivity = this.getActivity();
+        
+    }
+
+    public void testPreamble(){
+    	int preamble = BluetoothProtocol.PACKET_PREAMBLE & 0xFF;
+    	assertEquals(preamble, 0xAA);
+    }
+    
+    
+}
