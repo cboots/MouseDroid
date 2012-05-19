0b5ae070656af9ec7d51fcc9ea0f1f82fecd5a09
Collin Boots <cboots@seas.upenn.edu>
2012-05-19 03:11:25 +0000

    Added more protocol packet implementations. Tested mouse move and click functionality.

--- src/com/cfms/android/mousedroid/test/ProtocolTest.java
+++ src/com/cfms/android/mousedroid/test/ProtocolTest.java
@@ -3,7 +3,7 @@ package com.cfms.android.mousedroid.test;
 import android.test.ActivityInstrumentationTestCase2;
 
 import com.cfms.android.mousedroid.activity.PreferencesActivity;
-import com.cfms.android.mousedroid.bluetooth.BluetoothProtocol;
+import com.cfms.android.mousedroid.bluetooth.BTProtocol;
 
 public class ProtocolTest extends
 		ActivityInstrumentationTestCase2<PreferencesActivity> {
@@ -23,7 +23,7 @@ public class ProtocolTest extends
     }
 
     public void testPreamble(){
-    	int preamble = BluetoothProtocol.PACKET_PREAMBLE & 0xFF;
+    	int preamble = BTProtocol.PACKET_PREAMBLE & 0xFF;
     	assertEquals(preamble, 0xAA);
     }
     
